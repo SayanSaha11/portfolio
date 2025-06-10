dfc0e06c5187a17db11d92cb375445e8c8029a70
SayanSaha11 <perfectss1107@gmail.com>
2025-06-10 22:48:48 +0530

    Update ContactSection.jsx

--- src/Components/ContactSection.jsx
+++ src/Components/ContactSection.jsx
@@ -10,7 +10,7 @@ import {
   Youtube,
 } from "lucide-react";
 import { cn } from "@/lib/utils";
-import { useToast } from "@/hooks/use-toast";
+import { useToast } from "@/Hooks/use-toast";
 import { useState } from "react";
 
 export const ContactSection = () => {
